3f9fca05ebc39ba9b25c540f2d2453066c01e8dd
toyosaki <toyosaki2333@163.com>
2022-08-12 12:33:56 +0800

    完成了主页功能的开发

--- 素材/assets/js/baseAPI.js
+++ 素材/assets/js/baseAPI.js
@@ -4,5 +4,26 @@
 $.ajaxPrefilter(function(options) {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+
+      // 统一为有权限的接口，设置 headers 请求头
+  if (options.url.indexOf('/my/') !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
+
+
+  // 全局同意挂载complete函数
+  options.complete = function (res) {
+    // console.log('执行了completee回调')
+        // console.log(res)
+        // 在complete回调函数中，使用res.responseJASON函数
+        if(res.responseJSON.status === 1 && res.responseJSON.massage === '身份验证失败！'){
+          // 1.强制清空token
+          localStorage.removeItem('token')
+          // 2.强制跳转到登陆页面
+          location.href = '/login.html'
+  }
+}
   })
-  
+
